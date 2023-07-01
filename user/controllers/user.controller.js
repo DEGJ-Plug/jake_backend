119667c53db090428743681524b60730edcb4892
Dannyfsp <femoscopilala@gmail.com>
2023-07-01 13:55:01 +0100

    feat: updating authentication method

--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -1,12 +1,10 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
-const sendEmail = require('../../utils/email');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+const sendEmail = require("../../utils/email");
 
 const userSignUp = async (req, res) => {
-  let {
-    userName, email, password, role,
-  } = req.body;
+  let { userName, email, password, role } = req.body;
   userName = userName.toLowerCase();
   email = email.toLowerCase();
   try {
@@ -34,24 +32,24 @@ const userSignUp = async (req, res) => {
     const token = jwt.sign(
       { email, userId: newUser._id, userName },
       process.env.JWT_VERIFICATION_SECRET,
-      { expiresIn: process.env.JWT_VERIFICATION_EXP },
+      { expiresIn: process.env.JWT_VERIFICATION_EXP }
     );
 
     const verificationUrl = `http://localhost:${process.env.PORT}/auth/verify?token=${token}`;
     // DANNY do we have to wait until the mail is sent before the registration process ends ??
     await sendEmail({
       email,
-      subject: 'Account Verification',
-      text: '',
+      subject: "Account Verification",
+      text: "",
       html: `Hey ${userName},<p>Welcome to Jake, kindly <a href=${verificationUrl}>verify</a> your account<p>`,
-      message: 'Verification',
+      message: "Verification",
     });
 
     return res.status(201).json({
-      status: 'success',
+      status: "success",
       message: `${userName}, your account has been created successfully`,
       userId: newUser._id,
-      tokenExpiration: '48 hours',
+      tokenExpiration: "48 hours",
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -61,7 +59,9 @@ const userSignUp = async (req, res) => {
 const verifyUser = async (req, res) => {
   const { token } = req.query;
 
-  if (!token || typeof token !== 'string') { return res.status(400).json({ message: 'invalid request' }); }
+  if (!token || typeof token !== "string") {
+    return res.status(400).json({ message: "invalid request" });
+  }
 
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_VERIFICATION_SECRET);
@@ -77,26 +77,29 @@ const verifyUser = async (req, res) => {
     await user.save();
     await sendEmail({
       email: user.email,
-      subject: 'Welcome to Jake',
-      text: '',
+      subject: "Welcome to Jake",
+      text: "",
       html: `Hey ${user.userName},<p>Your account has been verified successfully.<p><p>Enjoy Jake</p>`,
-      message: 'Welcome to Jake',
+      message: "Welcome to Jake",
     });
     return res
       .status(200)
-      .json({ message: 'Your account has been verified successfully' });
+      .json({ message: "Your account has been verified successfully" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-const userLogin = async (req, res, next) => {
-  const { emailuserName, password } = req.body;
+const userLogin = async (req, res) => {
+  const { emailUserName, password } = req.body;
   try {
-    let userExists = await User.findOne({ email: emailuserName });
-    if (!userExists) {
-      userExists = await User.findOne({ userName: emailuserName });
-    }
+    let userExists = await User.findOne({
+      $or: [{ email: emailUserName }, { userName: emailUserName }],
+    });
+    if (!userExists)
+      return res
+        .status(400)
+        .json({ message: "user does not exist, please sign up" });
     if (userExists) {
       const validPassword = await bcrypt.compare(password, userExists.password);
       if (validPassword) {
@@ -107,9 +110,9 @@ const userLogin = async (req, res, next) => {
             userName: userExists.userName,
           },
           process.env.JWT_VERIFICATION_SECRET,
-          { expiresIn: process.env.JWT_LOGIN_EXP },
+          { expiresIn: process.env.JWT_LOGIN_EXP }
         );
-        res.cookie('token', token);
+        res.cookie("token", token);
         return res.status(200).json({
           success: true,
           data: {
@@ -118,7 +121,7 @@ const userLogin = async (req, res, next) => {
           },
         });
       }
-      return res.status(401).send({ error: 'Invalid credentials' });
+      return res.status(401).send({ error: "Invalid credentials" });
     }
   } catch (error) {
     return res.status(500).send({ error: error.message });
@@ -131,13 +134,18 @@ const profile = async (req, res) => {
   // const _exp = decode.exp * 1000;
   // const _date = new Date(_exp);
   // console.log(_date);
-  const existingUser = await User.findOne({ userName: user.userName, email: user.email });
+  const existingUser = await User.findOne({
+    userName: user.userName,
+    email: user.email,
+  });
   // console.log(existingUser);
   if (existingUser) {
-    return res.status(200).send({ message: `Welcome back ${user.userName}`, user });
+    return res
+      .status(200)
+      .send({ message: `Welcome back ${user.userName}`, user });
   }
-  res.clearCookie('token');
-  return res.status(401).redirect('/');
+  res.clearCookie("token");
+  return res.status(401).redirect("/");
 };
 module.exports = {
   userSignUp,
--- user/middlewares/tokenCookie.js
+++ user/middlewares/tokenCookie.js
@@ -1,14 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const validToken = (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies || req.headers.authorization.split(" ")[1];
   try {
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
     const user = jwt.verify(token, process.env.JWT_VERIFICATION_SECRET);
     req.user = user;
     return next();
   } catch (error) {
-    res.clearCookie('token');
-    return res.status(401).send({ error: 'Invalid user token' });
+    res.clearCookie("token");
+    return res.status(401).json({ error: "Unauthorized" });
     // return res.redirect('/');
   }
 };
