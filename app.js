827b7e3d237860f0be089f2353f05009bca9a7cf
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-07-16 16:39:04 +0100

    [DOCS] Initializing endpoint documentations

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { authrouter, userprofile } = require('./user/routes/user.route');
 const productRouter = require('./product/routes/product.route');
 const { requestReceived } = require('./utils/requestLogger');
+const swaggerDocument = require('./utils/swaggerDocs');
 
 const app = express();
 
@@ -11,6 +14,17 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestReceived({ logger: console.log }));
+
+// Load individual path files and merge them into the main document
+const greetingPath = YAML.load('./docs/paths/users.yaml');
+// const farewellPath = YAML.load('./docs/farewell.yaml');
+
+swaggerDocument.paths = {
+  ...swaggerDocument.paths,
+  ...greetingPath,
+};
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get(['/', '/home'], (_, res) => {
   console.log(new Date());
