425f5cf41edcb178a444f5474f780fd57fc29f6e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-07-17 08:10:43 +0100

    [DOCS] AUTH
    Created Login endpoint documentations

--- docs/index.js
+++ docs/index.js
@@ -2,13 +2,13 @@ const YAML = require('yamljs');
 const swaggerDocument = require('../utils/swaggerDocs');
 
 // Load individual path files and merge them into the main document
-const users = YAML.load('./docs/users/users.yaml');
 const register = YAML.load('./docs/auth/register.yml');
+const login = YAML.load('./docs/auth/login.yml');
 
 swaggerDocument.paths = {
   ...swaggerDocument.paths,
-  ...users,
   ...register,
+  ...login,
 };
 
 module.exports = swaggerDocument;
